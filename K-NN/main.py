1829294f5aa28b86ecf650b8a4d14877e8e5ec30
Alireza Heydari <alireza97hi@gmail.com>
2018-11-19 00:15:02 +0330

    normalize function added

--- K-NN/main.py
+++ K-NN/main.py
@@ -2,4 +2,5 @@ def euclidean_distance(x1, x2):
     return sum([(x1[i] - x2[i])**2 for i in range(len(x1))])**(1/2)
 
 
-print(euclidean_distance([1,2,3],[2,3,4]))
+def normalize(value, min_value, max_value):
+    return (value - min_value)/(max_value - min_value)
