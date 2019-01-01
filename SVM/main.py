87d0d9032342e4b3d7705c1138f72483bc1692d3
Alireza Heydari <alireza97hi@gmail.com>
2019-01-01 14:06:47 +0330

    SVM read data added

---
+++ SVM/main.py
@@ -0,0 +1,15 @@
+print(__doc__)
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
+
+
+data = pd.read_csv('svmdata.csv')
+labels = data.iloc[:,2]
+features = data.iloc[:,0:2]
+
+# plt.figure()
+# plt.scatter(features)
+
