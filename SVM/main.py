297e75008025af7a9d715fdee480beb3b2909c96
Alireza Heydari <alireza97hi@gmail.com>
2019-01-01 14:15:09 +0330

    train test validation set added

--- SVM/main.py
+++ SVM/main.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
 
 
 data = pd.read_csv('svmdata.csv')
@@ -14,4 +15,10 @@ plt.figure()
 plt.scatter(features.iloc[:,0],features.iloc[:,1], c=labels)
 
 plt.show()
+
+x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.4)
+
+x_test, x_validation, y_test, y_validation = train_test_split(x_test, y_test, test_size=0.5)
+
+
 
